324b61819456c008bee2484868efb828440b5739
Alessandro Ferraro <ale_ferraro@libero.it>
2020-11-05 15:46:29 +0100

    Dropdown on click
    Introduced script that open/close the dropdown menu on clicking

--- js/main.js
+++ js/main.js
@@ -1,8 +1,15 @@
 $(document).ready(function(){
+
+  //hover function
   $('.nav-right > ul > li').mouseenter(function(){
     $(this).children('.my_dropdown').toggle();
   })
   $('.nav-right > ul > li').mouseleave(function(){
     $(this).children('.my_dropdown').toggle();
+  })
+
+  // click function
+  $('.nav-right > ul > li').click(function(){
+    $(this).children('.my_dropdown').toggleClass('active');
   })
 })
